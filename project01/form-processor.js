34b01a6f9b87880808a1144581e1705c2a77f946
Ben Makuh <alphari@gmail.com>
2011-01-21 13:17:58 -0700

    Added a price column to the database.

--- project01/form-processor.js
+++ project01/form-processor.js
@@ -2,4 +2,5 @@ function feelBlank()
 {
     if (!document.getElementById("howYouFeel").value)
         alert("Surely you feel something!");
+    return false;
 }
--- project02/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Project02::Application.config.secret_token = 'efbe248d74970c79b760db2cf0552cad21ba398de7630a95d378352b4d61a69748e596cbed51acac0e7bba225957c086f54900f746d97925afc207fd523a6b02'
