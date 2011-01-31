df2ef3a1b8a9c54679f79bfcebb476a4ce73c376
Ben Makuh <alphari@gmail.com>
2011-01-30 22:52:23 -0700

    Iteration C2: creating a smarter cart

--- project02/config/initializers/session_store.rb
+++ project02/config/initializers/session_store.rb
@@ -5,11 +5,11 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_project02_session',
+  :key         => '_depot_session',
   :secret      => 'b22a409561fed748013dbefa5e79c77fe6097c069da8fcfa68b8b9615b55900cd3c72272e7475f051b5c4da1c648c187bfd5770acf8bd75a044edbb08bf3c0e3'
 }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_store = :active_record_store
