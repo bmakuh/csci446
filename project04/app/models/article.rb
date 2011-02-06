8216eaa362800bd778ce446b1cfd2caec8018213
Ben Makuh <alphari@gmail.com>
2011-02-06 14:17:47 -0700

    finished show view mostly.

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  validates_presence_of :title, :body, :author_name
+  
 end
--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -9,13 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110204214930) do
+ActiveRecord::Schema.define(:version => 20110206033107) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.string   "author_name"
-    t.string   "body"
-    t.string   "creation_date"
+    t.text     "body",          :limit => 255
+    t.date     "creation_date", :limit => 255, :default => '2011-02-05'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
