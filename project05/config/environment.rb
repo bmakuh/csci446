51610a5c6221a91ae8f849040521e978a532df1a
Ben Makuh <alphari@gmail.com>
2011-02-20 23:34:11 -0700

    HAML'd the _form partial and fixed a bug with the authors select list.

--- project05/config/environment.rb
+++ project05/config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   
   config.gem 'paperclip'
   config.gem 'will_paginate', :version => '~> 2.3.15', :source => 'http://gemcutter.org'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
